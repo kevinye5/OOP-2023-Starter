cec3d07b8e28924c089c0b68518e5bb45887b048
Kevin Ye <C21421254@mytudublin.ie>
2023-04-26 17:08:16 +0100

    code so far

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -19,13 +19,6 @@ public class Word {
         return follows;
     }
     
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(word).append(": ");
-        for (Follow follow : follows) {
-            builder.append(follow.toString()).append(", ");
-        }
-        return builder.substring(0, builder.length() - 2); // Remove the trailing comma and space
-    }
+   
 }
 
